e4a82825eba640a1b1cfc69c21256e390d041835
Davis Jhonns <davis.jhonns@gmail.com>
2025-10-30 04:21:24 +0100

    Implement RemoveMaterialDialog for selecting quantity to remove
    Purpose:
    
    The RemoveMaterialDialog is a simple form that allows users to specify the quantity of a material they wish to remove from the warehouse.
    
    Main Features:
    
    Quantity Input:
    
    A NumericUpDown control (nudQuantity) lets the user specify the quantity of the material to remove, with the value dynamically updated.
    
    Event Handlers:
    
    OK Button:
    
    When clicked, it closes the dialog and sets the result to DialogResult.OK, indicating the user is ready to proceed with the removal action.
    
    Cancel Button:
    
    When clicked, it closes the dialog without saving any input, setting the result to DialogResult.Cancel.
    
    Data Handling:
    
    The QuantityToDelete property returns the value selected in the NumericUpDown control, which can be used for the removal logic in the calling code.

---
+++ Tuzep/UI/Forms/DeleteMaterialDialog.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Tuzep.UI.Forms
+{
+    partial class RemoveMaterialDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            nudQuantity = new NumericUpDown();
+            btnOk = new Button();
+            btnCancel = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudQuantity).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(nudQuantity);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(96, 50);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Quantity";
+            // 
+            // nudQuantity
+            // 
+            nudQuantity.Dock = DockStyle.Fill;
+            nudQuantity.Location = new Point(3, 19);
+            nudQuantity.Name = "nudQuantity";
+            nudQuantity.Size = new Size(90, 23);
+            nudQuantity.TabIndex = 0;
+            nudQuantity.TextAlign = HorizontalAlignment.Center;
+            // 
+            // btnOk
+            // 
+            btnOk.Location = new Point(114, 10);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(93, 23);
+            btnOk.TabIndex = 1;
+            btnOk.Text = "OK";
+            btnOk.UseVisualStyleBackColor = true;
+            btnOk.Click += btnOk_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(114, 38);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(93, 23);
+            btnCancel.TabIndex = 1;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // RemoveMaterialDialog
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(214, 70);
+            Controls.Add(btnCancel);
+            Controls.Add(btnOk);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "RemoveMaterialDialog";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "DeleteMaterialDialog";
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)nudQuantity).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private NumericUpDown nudQuantity;
+        private Button btnOk;
+        private Button btnCancel;
+    }
+}
---
+++ Tuzep/UI/Forms/DeleteMaterialDialog.cs
@@ -0,0 +1,22 @@
+﻿namespace Tuzep.UI.Forms
+{
+    public partial class RemoveMaterialDialog : Form
+    {
+        public int QuantityToDelete => (int)nudQuantity.Value;
+        public RemoveMaterialDialog()
+        {
+            InitializeComponent();
+        }
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
